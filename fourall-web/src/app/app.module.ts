b91d608d1a40b0745b4788955f3e155f99f56885
Aklilu <aklilutsige@gmail.com>
2021-09-28 14:03:06 +0200

    Added header and user component with header navigation

--- fourall-web/src/app/app.module.ts
+++ fourall-web/src/app/app.module.ts
@@ -6,10 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppCMSMaterialModule } from './app-cms-material.module';
 
+import {HeaderComponent} from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { UserDeleteComponent } from './user/user-delete/user-delete.component';
+import { UserAddComponent } from './user/user-add/user-add.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    UserListComponent,
+    UserEditComponent,
+    UserDeleteComponent,
+    UserAddComponent
   ],
   imports: [
     BrowserModule,
